b87a132e8f98a4b855191b65b58b2367d482d075
longjianghu <215241062@qq.com>
2020-09-25 08:44:31 +0800

    调整默认参数

--- config/app.php
+++ config/app.php
@@ -4,8 +4,8 @@ return [
     'canal'     => [
         'host'        => env('CANAL_HOST', '127.0.0.1'),
         'port'        => env('CANAL_PORT', 11111),
-        'clientId'    => env('CANAL_CLIENT_ID', 'canal.client'),
-        'destination' => env('CANAL_DESTINATION', 'canal.server'),
+        'clientId'    => env('CANAL_CLIENT_ID', 10000),
+        'destination' => env('CANAL_DESTINATION', 'canal-server'),
         'filter'      => env('CANAL_FILTER', '.*\\..*'),
     ], // Canal 配置
     'serverUrl' => env('SERVER_URL'), // 远程提交URL
