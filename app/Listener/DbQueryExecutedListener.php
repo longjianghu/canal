8022fd8478e9315c01090b1c0fc29b989eae5d16
longjianghu <215241062@qq.com>
2022-01-19 14:17:01 +0800

    style:代码样式调整

--- app/Listener/DbQueryExecutedListener.php
+++ app/Listener/DbQueryExecutedListener.php
@@ -49,8 +49,9 @@ class DbQueryExecutedListener implements ListenerInterface
     {
         if ($event instanceof QueryExecuted) {
             $sql = $event->sql;
+
             if (! Arr::isAssoc($event->bindings)) {
-                foreach ($event->bindings as $key => $value) {
+                foreach ($event->bindings as $value) {
                     $sql = Str::replaceFirst('?', "'{$value}'", $sql);
                 }
             }
